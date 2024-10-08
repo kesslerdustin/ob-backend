bb05ce1192f3e6d02e0d8ca75c253ed4fa7f8f6d
@dustin.kessler <dustin.kessler@hs-ruhrwest.de>
2024-10-08 17:23:29 +0200

    firebase

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const admin = require('firebase-admin');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Initialize Firebase Admin SDK
+const serviceAccount = require('./keys/outdoor-bible-firebase-adminsdk-5kyx5-802e52f224.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://outdoor-bible.firebaseio.com' // Replace with your actual database URL
+});
 
 app.get('/', (req, res) => {
   res.send('Hello from the backend!');
