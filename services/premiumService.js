6c8d3fe4a7b022b3c40897b197c2f04bffcfa2e6
kesslerdustin <dustin.kessler@immotech.io>
2025-07-07 14:24:54 +0200

    new place - likes test2

--- services/premiumService.js
+++ services/premiumService.js
@@ -14,7 +14,7 @@ const CONSTANTS = {
   FREE_CREDIT_CAP: 50,              // Maximum credits free users can have to receive daily bonus
   
   // Location-related constants
-  FREE_DAILY_CUSTOM_LOCATIONS: 1,    // Daily custom location limit for free users
+  FREE_WEEKLY_CUSTOM_LOCATIONS: 3,    // Weekly custom location limit for free users
   PREMIUM_DAILY_CUSTOM_LOCATIONS: 10, // Daily custom location limit for premium users
   
   // Other feature limits can be added here
@@ -33,7 +33,7 @@ const CREDIT_LIMITS = {
 };
 
 const LOCATION_LIMITS = {
-  FREE_DAILY_CUSTOM_LOCATIONS: CONSTANTS.FREE_DAILY_CUSTOM_LOCATIONS,
+  FREE_WEEKLY_CUSTOM_LOCATIONS: CONSTANTS.FREE_WEEKLY_CUSTOM_LOCATIONS,
   PREMIUM_DAILY_CUSTOM_LOCATIONS: CONSTANTS.PREMIUM_DAILY_CUSTOM_LOCATIONS
 };
 
